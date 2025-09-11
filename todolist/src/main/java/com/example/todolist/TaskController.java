9ef41372bc947233f49e0f838cbb44059ca4cf6c
Dipak Mehata <dipakmehta9804@gmail.com>
2025-09-12 00:22:19 +0530

    Update CORS for frontend

--- todolist/src/main/java/com/example/todolist/TaskController.java
+++ todolist/src/main/java/com/example/todolist/TaskController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:5173") 
+@CrossOrigin(origins = "https://to-do-list-java-spring-boot.vercel.app")
 @RestController // Marks this class as a REST controller
 @RequestMapping("/api/tasks") // All endpoints in this class will start with /api/tasks
 public class TaskController {
